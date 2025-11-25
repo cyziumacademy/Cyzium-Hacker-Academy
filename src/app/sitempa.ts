03f3ed022099a5eee4ff44bfa1a57e30c4c96f01
cyziumacademy <cyziumacademy@gmail.com>
2025-11-25 08:20:52 +0530

    sitemap.ts created

---
+++ src/app/sitempa.ts
@@ -0,0 +1,24 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://www.cyziumacademy.com",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.cyziumacademy.com/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.cyziumacademy.com/contact",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.cyziumacademy.com/blogs",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.cyziumacademy.com/hackathon",
+      lastModified: new Date(),
+    },
+  ];
+}
