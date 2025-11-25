b905910cee1a9b66ee256fd29071631588f22c6a
cyziumacademy <cyziumacademy@gmail.com>
2025-11-25 08:30:01 +0530

    update

--- src/app/sitempa.ts
+++ src/app/sitempa.ts
@@ -1,23 +1,29 @@
-export default function sitemap() {
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://www.cyziumacademy.com",
+      url: "https://cyziumacademy.com/",
       lastModified: new Date(),
     },
     {
-      url: "https://www.cyziumacademy.com/about",
+      url: "https://cyziumacademy.com/about",
       lastModified: new Date(),
     },
     {
-      url: "https://www.cyziumacademy.com/contact",
+      url: "https://cyziumacademy.com/courses",
       lastModified: new Date(),
     },
     {
-      url: "https://www.cyziumacademy.com/blogs",
+      url: "https://cyziumacademy.com/blogs",
       lastModified: new Date(),
     },
     {
-      url: "https://www.cyziumacademy.com/hackathon",
+      url: "https://cyziumacademy.com/hackathon",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://cyziumacademy.com/contact",
       lastModified: new Date(),
     },
   ];
